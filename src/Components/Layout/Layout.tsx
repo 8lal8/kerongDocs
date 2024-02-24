46740ce150318aaf18eaaa6e59987fd14903fd27
Skiller <hellsong649@gmail.com>
2024-02-24 11:46:00 +0300

    merge

--- src/Components/Layout/Layout.tsx
+++ src/Components/Layout/Layout.tsx
@@ -2,8 +2,9 @@ import './index.scss'
 import Menu from '../Menu/Menu'
 import Header from '../Header/Header'
 import React from 'react'
-import bgImage from '../assets/split_background.png'
 import { Outlet, useLocation } from 'react-router-dom'
+import {ReactComponent as KerongLogo} from "../assets/kerong-logo-no-space.svg";
+import {ReactComponent as LinkLogo} from "../assets/icon_place.svg";
 const Layout = () => {
     const location = useLocation()
     const headerText = () => {
@@ -17,6 +18,70 @@ const Layout = () => {
                 break
             case '/kerong-lcs':
                 text = 'KERONG LCS'
+                break
+
+            case '/':
+                text = 'Немного терминологии..'
+                break
+            case '/auth-token':
+                text = 'Аутентификация в проекте'
+                break
+            case '/get-users':
+                text = 'Метод получения списка пользователей'
+                break
+            case '/get-user-id':
+                text = 'Метод получения пользователя по его идентификатору'
+                break
+            case '/get-roles':
+                text = 'Метод получения списка ролей'
+                break
+            case '/get-role-id':
+                text = 'Метод получения списка ролей по id пользователя'
+                break
+            case '/post-user':
+                text = 'Метод создания нового пользователя'
+                break
+            case '/post-role':
+                text = 'Метод создания новой роли'
+                break
+            case '/del-user':
+                text = 'Метод "удаления" пользователя'
+                break
+            case '/patch-role':
+                text = 'Метод изменения роли'
+                break
+            case '/patch-user':
+                text = 'Метод изменения пользователя'
+                break
+            case '/obj':
+                text = 'Описание объектов используемых в KERONG API'
+                break
+            case '/cu':
+                text = 'Методы для взаимодействия с платой KR-CU'
+                break
+            case '/bu':
+                text = 'Методы для взаимодействия с платой KR-BU'
+                break
+            case '/locks':
+                text = 'Методы для взаимодействия с замками'
+                break
+            case '/cu-16':
+                text = 'Подключение и настройка платы KR-CU 16'
+                break
+            case '/pass-cu-16':
+                text = 'Паспорт платы KR-CU 16'
+                break
+            case '/cu-48':
+                text = 'Подключение и настройка платы KR-CU 48'
+                break
+            case '/pass-cu-48':
+                text = 'Паспорт платы KR-CU 48'
+                break
+            case '/cu-48b':
+                text = 'Подключение и настройка платы KR-CU 48 B'
+                break
+            case '/pass-cu-48b':
+                text = 'Паспорт платы KR-CU 48 B'
                 break
         }
         return text
@@ -39,8 +104,7 @@ const Layout = () => {
                     backgroundColor: '#282c34',
                     borderRadius: '10px',
                     backgroundSize: 'cover',
-                    margin: '20px',
-                    marginLeft: '360px',
+                    marginLeft: '320px',
                     minHeight: '100vh',
                 }}
             >
@@ -58,10 +122,60 @@ const Layout = () => {
                             height: '100%',
                             paddingRight: '20px',
                             paddingLeft: '40px',
-                            paddingBottom: '20px',
+                            paddingTop: '120px',
+                            marginBottom: "20px",
+                            minHeight: "60vh"
                         }}
                     >
                         <Outlet />
+                    </div>
+
+                </div>
+                <div
+                    style={{
+                        display:"flex",
+                        justifyContent: "space-between",
+                        flexDirection: "row",
+                        alignItems: "start",
+                        color: 'white',
+                        flex: 1,
+                        height: '100%',
+                        backgroundColor: "#20232a",
+                        marginBottom: 0,
+                        minHeight: "120px",
+                        padding:"35px"
+                    }}
+                >
+                    <div style={{display:"flex", justifyContent: "flex-start", flexDirection: "row"}}>
+
+                    <div style={{display:"flex", flexDirection:"column", justifyContent: "start", padding:"10px", paddingLeft: "30px"}}>
+                        <div style={{margin: 0, padding: 0}}><h4 style={{margin:3, padding:0, color: "#999"}}>Может быть интересно</h4></div>
+                        <div style={{display: "flex", alignItems: "center"}}><h5 style={{margin:3, padding:0}}>Тестовая среда(Swagger)</h5><LinkLogo style={{height: "20px", width:"20px"}}/></div>
+                        <div style={{display: "flex", alignItems: "center"}}><h5 style={{margin:3, padding:0}}>dev.kerong.ru</h5><LinkLogo style={{height: "20px", width:"20px"}}/></div>
+                        <div style={{display: "flex", alignItems: "center"}}><h5 style={{margin:3, padding:0}}>kerong.ru</h5><LinkLogo style={{height: "20px", width:"20px"}}/></div>
+
+                    </div>
+                    <div style={{display:"flex", flexDirection:"column", justifyContent: "start", padding:"10px", paddingLeft: "30px"}}>
+                        <div style={{margin: 0, padding: 0}}><h4 style={{margin:3, padding:0, color: "#999"}}>ПО и оборудование</h4></div>
+                        <div style={{display: "flex", alignItems: "center"}}><h5 style={{margin:3, padding:0}}>Приобрести API</h5><LinkLogo style={{height: "20px", width:"20px"}}/></div>
+                        <div style={{display: "flex", alignItems: "center"}}><h5 style={{margin:3, padding:0}}>Платы KR-BU</h5><LinkLogo style={{height: "20px", width:"20px"}}/></div>
+                        <div style={{display: "flex", alignItems: "center"}}><h5 style={{margin:3, padding:0}}>Платы KR-CU</h5><LinkLogo style={{height: "20px", width:"20px"}}/></div>
+                        <div style={{display: "flex", alignItems: "center"}}><h5 style={{margin:3, padding:0}}>Каталог замков</h5><LinkLogo style={{height: "20px", width:"20px"}}/></div>
+                    </div>
+                    <div style={{display:"flex", flexDirection:"column", justifyContent: "start", padding:"10px", paddingLeft: "30px"}}>
+                        <div style={{margin: 0, padding: 0}}><h4 style={{margin:3, padding:0, color: "#999"}}>Информация</h4></div>
+                        <div style={{display: "flex", alignItems: "center"}}><h5 style={{margin:3, padding:0}}>Задать технический вопрос</h5><LinkLogo style={{height: "20px", width:"20px"}}/></div>
+                        <div style={{display: "flex", alignItems: "center"}}><h5 style={{margin:3, padding:0}}>Вопросы по приобретению</h5><LinkLogo style={{height: "20px", width:"20px"}}/></div>
+                        <div style={{display: "flex", alignItems: "center"}}><h5 style={{margin:3, padding:0}}>Разработка под ключ</h5><LinkLogo style={{height: "20px", width:"20px"}}/></div>
+                        <div style={{display: "flex", alignItems: "center"}}><h5 style={{margin:3, padding:0}}>Нашли ошибку?</h5><LinkLogo style={{height: "20px", width:"20px"}}/></div>
+
+                    </div>
+</div>
+
+
+                    <div style={{display:"flex", flexDirection:"column", justifyContent: "start", padding:"10px", paddingLeft: "30px", maxWidth:"320px"}}>
+                        <div style={{display: "flex", alignItems: "center"}}><KerongLogo style={{color: "white"}}/></div>
+                        <div style={{display: "flex", alignItems: "center"}}><h5>© 2024 KERONG Россия. Официальный дистрибьютор Kerong на территории России и СНГ.</h5></div>
                     </div>
                 </div>
             </div>
